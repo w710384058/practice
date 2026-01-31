632f4e8cc335191f01a4eb11194ce33ebb41388b
unknown <w710384058@gmail.com>
2026-01-31 16:15:04 +0800

    Delegate Exercises

--- kimmy/Program.cs
+++ kimmy/Program.cs
@@ -1,28 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using kimmy;
 
+delegate void Notify();
+
 class Program
 {
+    static void SayHello()
+    {
+        Console.WriteLine("Hello World!");
+    }
     static void Main()
     {
-        while (true)
-        {
-            Console.WriteLine("請輸入一個正整數：");
-            string input = Console.ReadLine() ?? "";//左邊如果=null,右邊會直接給空的字串
-
-            if (!double.TryParse(input, out double value))
-            {
-                Console.WriteLine("請輸入數字");
-                continue;//直接回while(true)
-            }
-            if (value <= 0)
-            {
-                Console.WriteLine("請輸入一個大於零的數字!");
-                continue;
-            }
-
-            Console.WriteLine($"你輸入的數字是{value}");
-            break;
-        }
+        Notify notify = SayHello;
+        notify();
+        Console.ReadLine();
     } 
 }
